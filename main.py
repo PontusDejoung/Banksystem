5f59063b46d28ab7184725f3a226f8b4fbe24582
PontusDejoung <pontusdejounge@gmail.com>
2023-08-27 10:00:04 +0200

    Changed to dict and list comprenhension

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ class Customer:
         self.account_number = account_number
         self.balance = balance
 
-def create_customers(num_customer)-> str: 
-    customers = []
+def create_customers_with_dict(num_customer)-> str: 
+    customers = {}
     start_time = time()
     for i in range(1,num_customer):
         account_number = f'1111-{i:010d}'
         customer = Customer(f'Customer {i}', '2000-01-01', account_number,0)
-        customers.append(customer)
+        customers[account_number] = customer
     end_time = time()
     elapsed_time = (end_time - start_time) * 1000
     print(f"Created {num_customer} customers in {elapsed_time:.2f} ms")
@@ -23,22 +23,20 @@ def create_customers(num_customer)-> str:
 
 def find_customer_by_account(customers, account_number_to_find)->str:
     start_time = time()
-    for customer in customers:
-        if customer.account_number == account_number_to_find:
-            end_time = time()
-            elapsed_time = (end_time - start_time) * 1000
-            print(f"Found customer {account_number_to_find} in {elapsed_time:.2f} ms")
-            return
-    end_time = time()
-    elapsed_time = (end_time - start_time) * 1000
-    print(f"Customer {account_number_to_find} not found in {elapsed_time:.2f} ms")
+    customer = customers.get(account_number_to_find)
+    if customer:
+        end_time = time()
+        elapsed_time = (end_time - start_time) * 1000
+        print(f"Found customer {account_number_to_find} in {elapsed_time:.2f} ms")
+    else:
+        end_time = time()
+        elapsed_time = (end_time - start_time) * 1000
+        print(f"Customer not found {account_number_to_find} in {elapsed_time:.2f} ms")
+    return customer
 
 if __name__ == "__main__":
-    num_customers = 10000000
-    customers = create_customers(num_customers)
+    customers_dict = create_customers_with_dict(10000000)
 
-    find_account = "1111-0000010000"
-    find_customer_by_account(customers, find_account)
-
-    non_existent_account = "1111-9999999999"
-    find_customer_by_account(customers, non_existent_account)
+    find_customer_by_account(customers_dict, '1111-0000001000')
+    find_customer_by_account(customers_dict, '1111-0009999999')
+    find_customer_by_account(customers_dict, '1111-9999999999')
