4bd3756dc0269ea82c4e94ffd2c60ebb25f61bc2
PontusDejoung <pontusdejounge@gmail.com>
2023-09-02 12:18:19 +0200

    la till CustomerRegistry

--- main.py
+++ main.py
@@ -1,44 +1,66 @@
-from time import time
+import time
 
 class Customer:
-    def __init__(self,name,birthdate,account_number,balance):
-        self.created = time()
+    def __init__(self, name, birthdate, account_no, balance=0):
+        self.created = time.time()
         self.last_updated = self.created
         self.name = name
         self.birthdate = birthdate
-        self.account_number = account_number
+        self.account_no = account_no
         self.balance = balance
 
-def create_customers(num_customer)-> str: 
-    customers = []
-    start_time = time()
-    for i in range(1,num_customer):
-        account_number = f'1111-{i:010d}'
-        customer = Customer(f'Customer {i}', '2000-01-01', account_number,0)
-        customers.append(customer)
-    end_time = time()
-    elapsed_time = (end_time - start_time) * 1000
-    print(f"Created {num_customer} customers in {elapsed_time:.2f} ms")
-    return customers
+class CustomerRegistry:
+    def __init__(self):
+        self.customer_list = []
 
-def find_customer_by_account(customers, account_number_to_find)->str:
-    start_time = time()
-    for customer in customers:
-        if customer.account_number == account_number_to_find:
-            end_time = time()
-            elapsed_time = (end_time - start_time) * 1000
-            print(f"Found customer {account_number_to_find} in {elapsed_time:.2f} ms")
-            return
-    end_time = time()
-    elapsed_time = (end_time - start_time) * 1000
-    print(f"Customer {account_number_to_find} not found in {elapsed_time:.2f} ms")
+    def add_customer(self, customer):
+        self.customer_list.append(customer)
+
+    def get_customer_by_account(self, account_no):
+        for customer in self.customer_list:
+            if customer.account_no == account_no:
+                return customer
+        return None
+
+def create_customer_list(num_customers):
+    start_time = time.time()
+    registry = CustomerRegistry()
+    for i in range(1, num_customers + 1):
+        account_no = f'1111-{i:010}'
+        customer = Customer(f'Customer {i}', f'01/01/2000', account_no)
+        registry.add_customer(customer)
+    
+    end_time = time.time()
+    elapsed_time = (end_time - start_time) * 1000 
+    return registry, elapsed_time
+
+def search_customer(account_no, registry):
+    start_time = time.time()
+    customer = registry.get_customer_by_account(account_no)
+    end_time = time.time()
+    elapsed_time = (end_time - start_time) * 1000  
+    return customer, elapsed_time
+
 
 if __name__ == "__main__":
+
+    # Skapa 10 miljoner kunder och mäta tiden
     num_customers = 10000000
-    customers = create_customers(num_customers)
+    customer_registry, creation_time = create_customer_list(num_customers)
+    print(f"Time to create {num_customers} customers: {creation_time:.2f} ms")
 
-    find_account = "1111-0000010000"
-    find_customer_by_account(customers, find_account)
+    # Sök efter ett specifikt konto (1111-0000001000)
+    search_account = '1111-0000001000'
+    found_customer, search_time = search_customer(search_account, customer_registry)
+    if found_customer:
+        print(f"Found account {search_account} for {found_customer.name} in {search_time:.2f} ms")
+    else:
+        print(f"Account {search_account} not found in {search_time:.2f} ms")
 
-    non_existent_account = "1111-9999999999"
-    find_customer_by_account(customers, non_existent_account)
+    # Sök efter ett konto som inte finns (1111-9999999999)
+    search_account = '1111-9999999999'
+    found_customer, search_time = search_customer(search_account, customer_registry)
+    if found_customer:
+        print(f"Found account {search_account} for {found_customer.name} in {search_time:.2f} ms")
+    else:
+        print(f"Account {search_account} not found in {search_time:.2f} ms")
