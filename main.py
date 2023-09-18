6e3fd35f0f6ed8b9af955c3433b7488709e435d1
PontusDejoung <pontusdejounge@gmail.com>
2023-09-18 19:29:41 +0200

    changes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from time import time
 from dataclasses import dataclass
 from decorators import timing_decorator
 import random
+from datetime import datetime
 
 @dataclass
 class Customer:
@@ -9,58 +10,48 @@ class Customer:
     birthdate: str
     account_number: str
     balance: float
-    created: float = None
-    last_updated: float = None
-
-def generate_account_numbers(num_customers):
-    account_numbers = []
-    for i in range(1, num_customers):
-        account_number = random.randint(1, 10_000_000)
-        if account_number not in account_numbers:
-            account_number_str = f'1111-{account_number:010d}'
-            account_numbers.append(account_number_str)
-
-    return account_numbers
-
-def quicksort(arr):
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[len(arr) // 2].account_number
-    left = [x for x in arr if x.account_number < pivot]
-    middle = [x for x in arr if x.account_number == pivot]
-    right = [x for x in arr if x.account_number > pivot]
-    return quicksort(left) + middle + quicksort(right)
+    created: str = None
+    last_updated: str = None
 
 @timing_decorator
-def create_customers_with_list(num_customers):
-    account_numbers = generate_account_numbers(num_customers)
-    current_time = time()
-    customers = [
-        Customer(
-            name=f'Customer {account_number}',
-            birthdate='2000-01-01',
-            account_number=account_number,
-            balance=0,
-            created=current_time,
-            last_updated=current_time
-        ) for account_number in account_numbers
+def generate_account_numbers_in_random_order(num_customers):
+    timestamp = datetime.now()
+    account_numbers = set()
+    customer_list = [
+        Customer(f'Customer{i}', f'01/01/1990', f'1111-{account_number:010d}', 0,timestamp,timestamp)
+        for i, account_number in enumerate(random.sample(range(1, 10_000_0000 + 1), num_customers))
+        if f'1111-{account_number:010d}' not in account_numbers and account_numbers.add(f'1111-{account_number:010d}') is None
     ]
-    return customers
 
-def find_customer_by_account(customers, account_number_to_find):
-    for customer in customers:
-        if customer.account_number == account_number_to_find:
-            print(f"Found customer {account_number_to_find}")
-            return
-    print(f"Customer not found {account_number_to_find}")
+    return customer_list
+
+def quicksort(arr, low, high):
+    if low < high:
+        pivot_index = partition(arr, low, high)
+        quicksort(arr, low, pivot_index)
+        quicksort(arr, pivot_index + 1, high)
+
+def partition(arr, low, high):
+    pivot = arr[(low + high) // 2].account_number
+    i = low - 1
+    j = high + 1
+    while True:
+        i += 1
+        while arr[i].account_number < pivot:
+            i += 1
+        j -= 1
+        while arr[j].account_number > pivot:
+            j -= 1
+        if i >= j:
+            return j
+        arr[i], arr[j] = arr[j], arr[i]
 
 if __name__ == "__main__":
-    customers_list = create_customers_with_list(10000)
+    customers_list = generate_account_numbers_in_random_order(10_000)
     start = time()
-    sort_customers = quicksort(customers_list)
+    sort_customers = quicksort(customers_list,0,len(customers_list) - 1)
     end = time()
     sorting_time = (end - start) * 1000
     print(f"It took {sorting_time:.2f}ms to sort ")
-    print(f"Första kundens kontonummer: {sort_customers[0].account_number}")
-    print(f"Sista kundens kontonummer: {sort_customers[-1].account_number}")
-    
+    print(f"Första kundens kontonummer: {customers_list[0].account_number}")
+    print(f"Sista kundens kontonummer: {customers_list[-1].account_number}")
