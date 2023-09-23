6cc57209de53ba577e28a87e8024152e60fc09d2
PontusDejoung <pontusdejounge@gmail.com>
2023-09-23 15:56:16 +0200

    börjar se klart ut

--- main.py
+++ main.py
@@ -21,29 +21,7 @@ def generate_account_numbers_in_random_order(num_customers):
     for i, account_number in enumerate(random.sample(range(1, 10_000_000 + 1), num_customers))
     ]
     return customer_list
-        
-def quicksort(arr, low, high):
-    if low < high:
-        pivot_index = partition(arr, low, high)
-        quicksort(arr, low, pivot_index)
-        quicksort(arr, pivot_index + 1, high)
-
-def partition(arr, low, high):
-    pivot = arr[(low + high) // 2].account_number
-    i = low - 1
-    j = high + 1
-    while True:
-        i += 1
-        while arr[i].account_number < pivot:
-            i += 1
-        j -= 1
-        while arr[j].account_number > pivot:
-            j -= 1
-        if i >= j:
-            return j
-        arr[i], arr[j] = arr[j], arr[i]
-
-
+ 
 @timing_decorator
 def binary_search(customers, account_number_to_find):
     left = 0
@@ -59,14 +37,36 @@ def binary_search(customers, account_number_to_find):
             right = mid - 1
     return None
 
-if __name__ == "__main__":
-    customers_list = generate_account_numbers_in_random_order(10_000_000)
-    start = time()
-    sort_customers = quicksort(customers_list,0,len(customers_list) - 1)
-    end = time()
-    sorting_time = (end - start) * 1000
-    print(f"It took {sorting_time:.2f}ms to sort ")
-    account_numbers_to_find = ["1111-0000001000", "1111-0009999999", "1111-9999999999"]
+def partition(array, start, end, compare_func):
+    pivot = array[start]
+    low = start + 1
+    high = end
+
+    while True:
+        while low <= high and compare_func(array[high], pivot):
+            high = high - 1
+
+        while low <= high and not compare_func(array[low], pivot):
+            low = low + 1
+
+        if low <= high:
+            array[low], array[high] = array[high], array[low]
+        else:
+            break
+
+    array[start], array[high] = array[high], array[start]
+
+    return high
+
+def quick_sort(array, start, end, compare_func):
+    if start >= end:
+        return
+
+    p = partition(array, start, end, compare_func)
+    quick_sort(array, start, p-1, compare_func)
+    quick_sort(array, p+1, end, compare_func)
+
+def find_customer(*account_numbers_to_find):
     results = [binary_search(customers_list, account_number) for account_number in account_numbers_to_find]
     for result, account_number in zip(results, account_numbers_to_find):
         if result:
@@ -74,6 +74,16 @@ if __name__ == "__main__":
         else:
             print(f"Kunde inte hitta kontonummer {account_number}")
 
+
+if __name__ == "__main__":
+    customers_list = generate_account_numbers_in_random_order(10_000_000)
+    start = time()
+    quick_sort(customers_list, 0, len(customers_list) - 1, lambda x, y: x.account_number > y.account_number)
+    end = time()
+    sorting_time = (end - start) * 1000
+    print(f"It took {sorting_time:.2f}ms to sort ")
     print(f"Första kundens kontonummer: {customers_list[0].account_number}")
     print(f"Sista kundens kontonummer: {customers_list[-1].account_number}")
+    find_customer("1111-0000001000", "1111-0009999999", "1111-9999999999")
+    
     
